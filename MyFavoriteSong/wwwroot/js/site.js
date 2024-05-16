d93c4c84aae9b1083065f9ff11f93907f9618491
yurijg@hotmail.com <JurijsGrigorjevs@users.noreply.github.com>
2024-05-16 01:41:42 +0100

    Add project files.

---
+++ MyFavoriteSong/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// flexing text
+const flex = document.getElementById('flex');
+let color = 'red';
+let intervalId;
+
+function changeColor() {
+    const colors = ['red', 'green', 'blue', 'grey', 'purple', 'violet'];
+    const index = colors.indexOf(color);
+    let nextIndex = (index + 1) % colors.length;
+    color = colors[nextIndex];
+    flex.style.color = color;
+}
+
+function startAnim() {
+    changeColor();
+    intervalId = setInterval(changeColor, 2000); // 2 sec
+}
+
+startAnim();
